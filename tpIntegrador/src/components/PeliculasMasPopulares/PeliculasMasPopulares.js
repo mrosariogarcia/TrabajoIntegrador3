c585be7c639e9f0b7acf95b3012ced2500999384
juanagrether <jgrether@udesa.edu.ar>
2024-09-17 09:12:02 -0300

    Punto 7 - Resultados de búsqueda

--- tpIntegrador/src/components/PeliculasMasPopulares/PeliculasMasPopulares.js
+++
@@ -1,4 +0,0 @@
-//import React, { Component } from 'react';
-//import PeliculasMasPopularesCard from '../PeliculasMasPopularesCard/PeliculasMasPopularesCard';
-
-//export default PeliculasMasPopularesCard;
--- tpIntegrador/src/pages/Home.js
+++
@@ -1,4 +0,0 @@
-//import React, { Component } from 'react';
-//import PeliculasMasPopulares from '../components/PeliculasMasPopulares/PeliculasMasPopulares';
-
-//export default Home;
